2bd0a5851d45aba23ce0218a623ecc0d0c1296ec
John Ryan <jnyryan@gmail.com>
2014-07-28 21:06:35 +0100

    removed docker as it's a seperate project

--- install.sh
+++ install.sh
@@ -6,13 +6,6 @@
 apt-get update
 apt-get install -y make curl git 
 
-
-#####################
-# Install docker
-sudo apt-get install -y docker.io
-sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
-sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
-
 #####################
 # Build and install logstash-forwarder 
 sudo apt-get install -y golang
